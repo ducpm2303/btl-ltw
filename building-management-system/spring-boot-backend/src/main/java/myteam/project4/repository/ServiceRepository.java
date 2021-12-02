61bd0ce7280aa00c21c5562ece62e90289a997d6
sonnhthtb <sonnhthtb1610@gmail.com>
2021-12-02 21:09:42 +0700

    Fix conflic

--- building-management-system/spring-boot-backend/src/main/java/myteam/project4/repository/ServiceRepository.java
+++ building-management-system/spring-boot-backend/src/main/java/myteam/project4/repository/ServiceRepository.java
@@ -1,7 +1,6 @@
 package myteam.project4.repository;
 
-import myteam.project4.entity.CleanedService;
-import myteam.project4.entity.Service;
+import myteam.project4.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,8 +11,21 @@ import java.util.Optional;
 public interface ServiceRepository extends JpaRepository<Service, Long> {
     Optional<CleanedService> findCleanedServiceByActiveIs(boolean active);
 
+    Optional<ParkingService> findParkingServiceByActiveIs(boolean active);
+
+    Optional<FoodService> findFoodServiceByActiveIs(boolean active);
+
+    Optional<ProtectedService> findProtectedServiceByActiveIs(boolean active);
+
     @Transactional
     @Modifying
-    @Query("UPDATE Service service SET service.active = false")
-    void deactivateAll();
+    @Query("UPDATE Service service SET service.active = false WHERE service.id IN (SELECT c.id FROM CleanedService c)")
+    void deactivateAllCleanedService();
+
+    Optional<MaintenanceService> findMaintenanceServiceByActiveIs(boolean active);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Service service SET service.active = false WHERE service.id IN (SELECT m.id FROM MaintenanceService m)")
+    void deactivateAllMaintenanceService();
 }
