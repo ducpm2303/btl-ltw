daeb1ee32d57026687e898d234a51a96eb87e462
sonnhthtb <sonnhthtb1610@gmail.com>
2021-12-08 22:34:41 +0700

    Add api

--- building-management-system/spring-boot-backend/src/main/java/myteam/project4/model/request/BuildingEmployeeRequest.java
+++ building-management-system/spring-boot-backend/src/main/java/myteam/project4/model/request/BuildingEmployeeRequest.java
@@ -2,17 +2,13 @@ package myteam.project4.model.request;
 
 
 import lombok.Data;
-import myteam.project4.entity.MonthSalary;
-import myteam.project4.entity.Salary;
 
 @Data
 public class BuildingEmployeeRequest {
-    private String code;
     private String name;
     private String dateOfBirth;
     private String address;
     private String phone;
     private String level;
     private String position;
-    private Float salary;
 }
