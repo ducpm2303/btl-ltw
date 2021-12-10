cab2dd47e00981acd202f75a7eff595980b8585a
ducpm2303 <p.m.d.2203@Gmail.com>
2021-12-11 00:29:54 +0700

    update delete usedservice

--- building-management-system/spring-boot-backend/src/main/java/myteam/project4/service/implement/UsedServiceBusinessServiceImpl.java
+++ building-management-system/spring-boot-backend/src/main/java/myteam/project4/service/implement/UsedServiceBusinessServiceImpl.java
@@ -2,17 +2,21 @@ package myteam.project4.service.implement;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import myteam.project4.entity.MonthUsedService;
 import myteam.project4.entity.UsedService;
-import myteam.project4.exception.BusinessCode;
-import myteam.project4.exception.BusinessException;
 import myteam.project4.mapper.UsedServiceMapper;
 import myteam.project4.model.request.UsedServiceRequest;
 import myteam.project4.model.response.UsedServiceResponse;
+import myteam.project4.repository.MonthUsedServiceRepository;
 import myteam.project4.repository.UsedServiceRepository;
 import myteam.project4.service.UsedServiceBusinessService;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,12 +26,19 @@ import java.util.stream.Collectors;
 public class UsedServiceBusinessServiceImpl implements UsedServiceBusinessService {
 
     private final UsedServiceRepository repository;
+    private final MonthUsedServiceRepository monthUsedServiceRepository;
     private final UsedServiceMapper mapper;
 
     @Override
     public UsedServiceResponse save(UsedServiceRequest request) {
-        UsedService usedService = mapper.to(request);
-        return mapper.to(repository.saveAndFlush(usedService));
+        UsedService usedService = repository.saveAndFlush(mapper.to(request));
+        MonthUsedService monthUsedService = new MonthUsedService();
+        Date toDate = Date.valueOf(LocalDate.now().withDayOfMonth(1));
+        monthUsedService.setFromDate(new Timestamp(System.currentTimeMillis()));
+        monthUsedService.setUsedService(usedService);
+        monthUsedService.setToDate(new Timestamp(DateUtils.addMonths(toDate,1).getTime()));
+        monthUsedServiceRepository.saveAndFlush(monthUsedService);
+        return mapper.to(usedService);
     }
 
     @Override
